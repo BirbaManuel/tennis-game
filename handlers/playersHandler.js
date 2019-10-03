5a99b469d870e264328657bb89b883f0e1a77b07
BirbaManuel <birba@et.esiea.fr>
2019-10-03 21:11:34 +0200

    add try catch in handlers

--- handlers/playersHandler.js
+++ handlers/playersHandler.js
@@ -4,18 +4,28 @@ const PLAYERS_JSON_FILE = require("../data/headtohead.json")
 const PLAYERS_URL = "https://eurosportdigital.github.io/eurosport-node-developer-recruitment/headtohead.json"
 /* HANDLERS */
 async function getPlayers (){
-    const playersFromENdPoint = await fetch(PLAYERS_URL)
-    const players = (playersFromENdPoint.status === 200) && await playersFromENdPoint.json()
+    try {
+        const playersFromENdPoint = await fetch(PLAYERS_URL)
+        const players = (playersFromENdPoint.status === 200) && await playersFromENdPoint.json()
 
-    // retreive players list from local json file
-    const staticPlayers = PLAYERS_JSON_FILE
-    const resultat = (players.players.length >= 0 ) ? players : staticPlayers
-    return resultat
+        // retreive players list from local json file
+        const staticPlayers = PLAYERS_JSON_FILE
+        const resultat = (players.players.length >= 0 ) ? players : staticPlayers
+        return resultat
+    } catch (error){
+        return error.message
+    }
 }
 
 async function getPlayer(id){
-	const { players : arrayOfPlayers } = await getPlayers()
-	return arrayOfPlayers.find( player => player.id == id)
+    try{
+        const { players : arrayOfPlayers } = await getPlayers()
+        const resultat =  arrayOfPlayers.find( player => player.id == id)
+        return resultat
+    } catch (error){
+
+        return error.message
+    }
 }
 /* HANDLERS */
 
